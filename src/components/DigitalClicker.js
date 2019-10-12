6b8a2cd6edc56566765dbb7bb3df633accc3d62c
kmarks2013 <kmarks2013@gmail.com>
2019-10-12 18:16:55 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
-// Code DigitalClicker Component Here
+// Code DigitalClicker Component Her
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+    state =  {
+        timesClicked: 0
+    }
+
+    clickHandler = () => {
+        console.log(this.state.timesClicked)
+        this.setState(prevState => ({
+            timesClicked: prevState.timesClicked+1
+        }))
+    }
+
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <button onClick={this.clickHandler}>{this.state.timesClicked}</button>
+                {/* i am a DigitalClicker component */}
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,50 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+
+    state = {
+        errors: [],
+        user: null,
+        settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+            }
+        }
+    }
+
+    bitrateHandler = () => {
+        console.log(this.state.settings.bitrate)
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    
+    resolutionHandler= () => {
+        console.log(this.state.settings.video)
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.bitrateHandler} className='bitrate'>Change Bitrate</button>
+                <button onClick={this.resolutionHandler} className='resolution'>Change Resolution</button>
+            </div>
+        )
+    }
+}
